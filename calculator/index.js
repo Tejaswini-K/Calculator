3222c9b6c3617f7117c00f39f495929bafd8a455
Tejaswini <58681247+Tejaswini-K@users.noreply.github.com>
2020-09-02 18:39:43 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,124 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = ''
+ for(item of buttons){
+      item.addEventListener('click',(e)=>{
+       buttonText = e.target.innerText;
+      console.log(buttonText);
+      if(buttonText == 'X'){
+            buttonText = '*'
+             screenValue += buttonText;
+            screen.value = screenValue;
+}
+       else if(buttonText == 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+}
+       else if(buttonText == '='){
+       screen.value = eval(screenValue);
+       setTimeout(()=>screen.value.classList.remove('screenValue'),500);
+
+}
+else{
+         screenValue += buttonText;
+            screen.value = screenValue;
+
+
+}
+})
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
